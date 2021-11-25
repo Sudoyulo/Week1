f320a645fb57d8cc1ec3e3cda743749170bec33a
Kevin Tan Yulo <yulokevin@gmail.com>
2021-11-24 18:35:54 -0800

    refactor with mentor

--- lotide/eqObjects.js
+++ lotide/eqObjects.js
@@ -19,25 +19,16 @@ const eqArrays = function(array1, array2) {
 };
 
 const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
+  if (Object.keys(object1).length !== Object.keys(object2).length)
     return false;
-  }
-  let found = false; //flag
+  
   for (let o1name in object1) {
-    found = false;
     for (let o2name in object2) {
-      if (o1name === o2name) {
-        if (typeof object1[o1name] === "object" && typeof object2[o2name] === "object") {
-          if (eqArrays(object1[o1name],object2[o2name])) {
-            found = true;
-          }
-        } else if (object1[o1name] === object2[o2name]) {
-          found = true;
+      if (o1name === o2name && (typeof object1[o1name] === "object" && typeof object2[o2name] === "object")) {
+        if (!eqArrays(object1[o1name],object2[o2name])) {
+          return false;
         }
       }
-    }
-    if (found === false) {
-      return false;
     }
   }
   return true;
