c91d15f763523eb9adca74b1b71bb8671da43809
Kevin Tan Yulo <yulokevin@gmail.com>
2021-11-24 17:58:06 -0800

    assertObjEqual

--- lotide/assertObjectsEqual.js
+++ lotide/assertObjectsEqual.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: 😁 ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: 😕 ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -43,16 +35,31 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba),true); // => true
 
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc),false); // => false
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
+const obj1 = {
+  name: "Kevin",
+  location: "Canada"
+};
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
+const obj2 = {
+  name: "Devin",
+  location: "Canada"
+};
+
+const obj3 = {
+  location: "Canada",
+  name: "Kevin"
+};
+
+assertObjectsEqual(obj1,obj1);
+assertObjectsEqual(obj1,obj2);
+assertObjectsEqual(obj1,obj3);
