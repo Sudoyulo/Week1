6088c66451d826d9e9c26211035283d6db19d403
Kevin Tan Yulo <yulokevin@gmail.com>
2021-11-25 17:00:55 -0800

    findKey

---
+++ lotide/findKey.js
@@ -0,0 +1,55 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ✅✅✅ ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: 🛑🛑🛑 ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (let name in object) {
+    if (callback(object[name]))
+      return name;
+  }
+};
+
+const test1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+
+const test2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 1);
+
+const test3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3);
+
+const test4 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4);
+
+assertEqual(test1,"noma");
+assertEqual(test2,"Blue Hill");
+assertEqual(test3,"Akaleri");
+assertEqual(test4,undefined);
